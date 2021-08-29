6580713c6f40ea29f055bfa351c6cf5784433640
therealbaljeet <chitgopekarsamarth@gmail.com>
2021-08-29 18:56:02 -0500

    Update render.js

--- assets/js/render.js
+++ assets/js/render.js
@@ -4,7 +4,7 @@ var dynamic = $("#dynamic");
 $(document).ready(function() {
     var url = window.location.href;
     var path = url.replace(base, "");
-    var projects = '<h2>projects 👨‍💻</>'+dynamic.html();
+    var projects = '<h2 style="margin-bottom:15px">projects 👨‍💻</h2>'+dynamic.html();
     if (!path)
         path = "README";
     var url = `https://raw.githubusercontent.com/http-samc/http-samc/main/PORTFOLIO/${path}.md`;
