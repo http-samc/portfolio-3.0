c6e2356d1fb84ea5b6a33e90993502124b329956
http-samc <67826352+http-samc@users.noreply.github.com>
2021-10-17 00:40:53 -0500

    added initial build

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// Run any other pre-build compilations you need here...
-const projects = require('./utils/gen-project-fragment');
-projects.genFragment();
-
-// Now, start off by building all html pages to ./public
-const render = require('./utils/render-md');
-render.render('content', 'public', 'templates/base.html')
-
 // Setting up Express
 const express = require('express');
 const fs = require('fs')
