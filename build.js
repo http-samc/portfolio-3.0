042efc43caa22cf8e57c6ec6e198457a88b96460
http-samc <67826352+http-samc@users.noreply.github.com>
2021-10-17 01:00:12 -0500

    Update build.js

--- build.js
+++ build.js
@@ -2,6 +2,6 @@
 const projects = require('./utils/gen-project-fragment');
 projects.genFragment();
 
-// Now, start off by building all html pages to ./public
+// Now, build all html pages to ./public
 const render = require('./utils/render-md');
 render.render('content', 'public', 'templates/base.html');
